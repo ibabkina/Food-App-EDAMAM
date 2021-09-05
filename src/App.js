e177f47e8a8d3de4166233a246e5bc04715d38ce
ibabkina <i.babkina700@gmail.com>
2021-09-05 15:16:20 -0400

    data is returned to console from DB on API get request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Axios from 'axios'
 import './App.css';
 
 const App = () => {
+    //Create a piece of state
+    const[query, setQuery] = useState(""); //Set query to empty string
+    const[recipes, setRecipes] = useState([]);
+
     const APP_ID = "4e9f05eb";
     const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
-    // const url = `/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    const headers = {
-        'Content-Type': 'text/plain'
-    };
+    
+    //To pass query in the url: ${query}
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
     const getData = async() => {
         const result = await Axios.get(url);
-       
+        setRecipes(result.data.hits)
         console.log(result);
-    }
+        setQuery("");
+    };
+
+    const onChange = (e) => {
+        setQuery(e.target.value);
+        // console.log(e.target.value);
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        getData();
+    };
+
     
     return (
         <div className='App'>
-            <h1 onClick={getData}>Food Searching App</h1>
+            <h1>Food Searching App</h1>
+            <form className="search-form" onSubmit={onSubmit}>
+                <input 
+                type="text" 
+                placeholder="Search Food" 
+                autocomplete="off" 
+                onChange={onChange}
+                // To clear the input field after submition
+                value={query}
+                />
+                <input type="submit" value="search"/>
+            </form>
         </div>
     )
 }
